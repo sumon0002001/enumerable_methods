32c0c6f2507250d0c9be88e0b5422e25df1023f8
kenderb <kenderboliar@gmail.com>
2020-10-22 10:30:44 -0500

    fix some indent errors

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -1,13 +1,12 @@
 # my_Enumerable
+# rubocobStyle/Documentation
 
 module Enumerable
-
-
   def my_each
     return to_enum(:my_each) unless block_given?
 
-    item = self if self.class == Array
-    item = to_a if self.class == Range || Hash
+    item = self if instance_of?(Array)
+    item = to_a if instance_of?(Range) || Hash
 
     i = 0
     while i < item.length
@@ -20,9 +19,10 @@ module Enumerable
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
 
-    item = self if self.class = Array
-    item = to_a if self.class = Range || Hash
+    item = self if instance_of?(Array)
+    item = to_a if instance_of?(Range) || Hash
     i = 0
+
     until i < item.length
       yield(item[i], i)
       i += 1
@@ -32,7 +32,7 @@ module Enumerable
 
   def my_select
     return to_enum(:my_select) unless block_given?
-    
+
     array = []
     my_each { |item| array << item if yield item }
     array
@@ -48,7 +48,8 @@ module Enumerable
       my_each { |item| return false unless [item.class, item.class.superclass].include?(argument) }
     elsif argument.class && !argument.nil == Regexp
       my_each { |item| return false unless argument.match(item) }
-    else my_each { |item| return false if item != argument }
+    else 
+      my_each { |item| return false if item != argument }
     end
     true
   end
@@ -57,36 +58,33 @@ module Enumerable
     if block_given?
       my_each { |item| return true if yield(item) }
       return false
-      if argument.nil?
+    elsif argument.nil?
       my_each { |item| return true if item }
-      elsif !argument.nil? && argument.is_a?(Class)
-        my_each { |item| return true unless [item.class, item.class.superclass].include?(argument) }
-      elsif argument.class && !argument.nil == Regexp
-        my_each { |item| return true unless argument.match(item) }
-      else
-        my_each { |item| return true if item == argument }
- 
+    elsif !argument.nil? && argument.is_a?(Class)
+      my_each { |item| return true unless [item.class, item.class.superclass].include?(argument) }
+    elsif argument.class && !argument.nil == Regexp
+      my_each { |item| return true unless argument.match(item) }
+    else
+      my_each { |item| return true if item == argument }
     end
     false
   end
 
   def my_none?(argument = nil, &block)
-    
     !my_any?(argument, &block)
   end
 
   def my_count(argument = nil)
     count = 0
     if !block_given? && argument.nil?
-      
       count = to_a.length
-     elsif block_given? 
-       my_each { |item| count += 1 if yield(item) } 
-     else 
-       count = my_select { |item| item == argument }.length
+    elsif block_given?
+      my_each { |item| count += 1 if yield(item) }
+    else
+      count = my_select { |item| item == argument }.length
     end
     count
-   end
+  end
 
   def my_map(parameter = nil)
     return to_enum(:my_map) unless block_given?
@@ -109,11 +107,11 @@ module Enumerable
       my_each { |item| argument = argument.nil? ? item : argument.send(sym, item) }
     else
       my_each { |item| argument = argument.nil? ? item : yield(argument, item) }
-      end
-      argument
-      end
     end
+    argument
+  end
 
-    def multiply_els(items)
+  def multiply_els(items)
     items.my_inject { |result, item| result * item }
   end
+end
