6eeb780c20715f1698ba637fc6913089964ec0dd
sumon0002001 <sumon0002001@gmail.com>
2020-10-23 00:07:15 +0600

    fixed my_each_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -20,8 +20,8 @@ module Enumerable
 
     item = to_a
     i = 0
-    while i < var.length
-      yield(var[i], i)
+    while i < item.length
+      yield(item[i], i)
       i += 1
     end
     self
@@ -113,3 +113,6 @@ end
 def multiply_els(items)
   items.my_inject { |result, item| result * item }
 end
+
+array = [2, 8, 0, 8, 1, 3, 2, 3, 2, 1, 5, 7, 0, 8, 4, 3, 7, 7, 3, 7, 5, 8, 2, 2, 6, 8, 0, 7, 7, 2, 5, 6, 7, 7, 5, 2, 3, 6, 2, 4, 8, 0, 5, 4, 0, 3, 3, 8, 5, 5, 5, 6, 0, 8, 4, 1, 5, 7, 1, 2, 7, 1, 3, 6, 4, 5, 3, 2, 7, 0, 5, 4, 1, 6, 1, 3, 2, 0, 7, 4, 2, 1, 8, 0, 3, 5, 6, 4, 5, 5, 3, 3, 4, 5, 4, 4, 7, 3, 5, 3] 
+p array.my_each_with_index { |num| num + 1 }
